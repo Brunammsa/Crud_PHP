20988114eeaaa890492daf83fe56278b1790caa9
Bruna <brunamelo.sa@gmail.com>
2023-02-28 17:36:29 -0300

    atualização namespace das classes

--- autoload.php
+++ autoload.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(function (string $nomeCompletoDaClasse){
-    $caminhoArquivo = str_replace('Bruna\\Classes', 'classes', $nomeCompletoDaClasse);
+    $caminhoArquivo = str_replace('Bruna\\Classes', 'Classes', $nomeCompletoDaClasse);
     $caminhoArquivo = str_replace('\\', DIRECTORY_SEPARATOR, $caminhoArquivo);
     $caminhoArquivo .= '.php';
 
